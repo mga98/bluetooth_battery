cc0303ba599ffae690b190e56ab5ba8026518ef9
Miguel <miguel.almeida1009@gmail.com>
2025-05-06 14:08:43 -0300

    new phone icon

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import os
 import gi
 import time
 import signal
@@ -10,25 +11,27 @@ gi.require_version('AppIndicator3', '0.1')
 
 from gi.repository import AppIndicator3, Gtk, GLib  # noqa
 
+currpath = os.path.dirname(os.path.realpath(__file__))
+
 
 class BatteryIndicator:
     def __init__(self):
         self.indicator = AppIndicator3.Indicator.new(
-            "bt_battery",
-            "audio-headphones-symbolic",
+            'bt_battery',
+            currpath + '/icons/white_headphone.png',
             AppIndicator3.IndicatorCategory.APPLICATION_STATUS
         )
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
 
         self.menu = Gtk.Menu()
-        self.battery_item = Gtk.MenuItem(label="Bateria do fone: ...%")
+        self.battery_item = Gtk.MenuItem(label='Bateria do fone: ...%')
         self.battery_item.set_sensitive(False)
 
         self.menu.append(self.battery_item)
 
         self.menu.append(Gtk.SeparatorMenuItem())
-        quit_item = Gtk.MenuItem(label="Sair")
-        quit_item.connect("activate", self.quit)
+        quit_item = Gtk.MenuItem(label='Sair')
+        quit_item.connect('activate', self.quit)
         self.menu.append(quit_item)
 
         self.menu.show_all()
@@ -43,26 +46,30 @@ class BatteryIndicator:
         self.update_thread.start()
 
     def update_battery_loop(self) -> None:
-        """
+        '''
         Faz o update da interface gráfica para atualizar o nível
         da bateria a cada 5 segundos
-        """
+        '''
         while self.running:
             device = get_battery_level()
 
             if device:
                 GLib.idle_add(
                     self.battery_item.set_label,
-                    f"{device['device_name']}: {device['battery_life']}%"
+                    f'{device['device_name']}: {device['battery_life']}%'
                 )
                 GLib.idle_add(
                     self.indicator.set_label,
-                    f"{device['battery_life']}%", ''
+                    f'{device['battery_life']}%', ''
                 )
             else:
                 GLib.idle_add(
                     self.battery_item.set_label,
-                    "Fone não encontrado"
+                    'Fone não encontrado'
+                )
+                GLib.idle_add(
+                    self.indicator.set_label,
+                    '', ''
                 )
 
             time.sleep(5)
@@ -78,5 +85,5 @@ def main():
     Gtk.main()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
